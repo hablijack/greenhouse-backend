0621eed726ea0de63c66d56e8236342af74e8b95
hablijack <christoph.habel@posteo.de>
2022-09-10 17:31:49 +0200

    added sensors as db-import

--- src/main/java/de/hablijack/greenhouse/service/SensorService.java
+++ src/main/java/de/hablijack/greenhouse/service/SensorService.java
@@ -1,35 +1,17 @@
 package de.hablijack.greenhouse.service;
 
-import com.influxdb.client.InfluxDBClient;
-import com.influxdb.client.InfluxDBClientFactory;
-import com.influxdb.client.QueryApi;
-import com.influxdb.query.dsl.Flux;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
-import javax.annotation.PostConstruct;
+import io.quarkus.vertx.ConsumeEvent;
+import java.io.IOException;
 import javax.enterprise.context.ApplicationScoped;
-import javax.xml.crypto.Data;
+import javax.transaction.Transactional;
+import javax.xml.stream.XMLStreamException;
 
 @ApplicationScoped
-public class SensorService implements AutoCloseable {
-  private InfluxDBClient influxDBClient;
+public class SensorService {
 
-  @PostConstruct
-  private void initializeInfluxDBClient() {
-    this.influxDBClient =
-        InfluxDBClientFactory.create("http://yggdrasil.fritz.box:8086",
-            "SAm8mju6EYr8vGlOBrqYQIMcrgwEID1Gxo8nBhCX4ucBigeM-XuHQSwCeA9xECVbB1rUC5dWIChEnDnVNRn_Yg==".toCharArray(),
-            "habel");
-  }
+  @Transactional
+  @ConsumeEvent(value = "fetch_feed_information", blocking = true)
+  public void fetchFeedInformation() throws IOException, XMLStreamException {
 
-  @Override
-  public void close() throws Exception {
-    this.influxDBClient.close();
-  }
-
-  public List<Data> getAllData() {
-    String temperatureByTimeQuery = Flux.from("greenhouse").range(-14L, ChronoUnit.DAYS).toString();
-    QueryApi queryApi = influxDBClient.getQueryApi();
-    return queryApi.query(temperatureByTimeQuery, Data.class);
   }
 }
