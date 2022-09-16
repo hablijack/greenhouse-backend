4786d15c022984a26a4bd21be43ea0d1be691d86
hablijack <christoph.habel@posteo.de>
2022-09-16 07:56:32 +0200

    sockets working!

--- src/main/java/de/hablijack/greenhouse/api/sensor/MeasurementSocket.java
+++ src/main/java/de/hablijack/greenhouse/api/sensor/MeasurementSocket.java
@@ -1,16 +1,15 @@
 package de.hablijack.greenhouse.api.sensor;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.hablijack.greenhouse.service.SensorService;
 import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.websocket.OnClose;
-import javax.websocket.OnError;
+import javax.transaction.TransactionManager;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import org.eclipse.microprofile.context.ManagedExecutor;
 import org.jboss.logmanager.Level;
 
 @ServerEndpoint("/api/sensors/measurements/socket")
@@ -20,38 +19,29 @@ public class MeasurementSocket {
   private static final Logger LOGGER = Logger.getLogger(MeasurementSocket.class.getName());
 
   @Inject
+  ManagedExecutor managedExecutor;
+
+  @Inject
+  TransactionManager transactionManager;
+
+  @Inject
   SensorService sensorService;
 
   @OnOpen
-  public void onOpen(Session session) throws JsonProcessingException {
-    try {
-      ObjectMapper mapper = new ObjectMapper();
-      String value = mapper.writeValueAsString(sensorService.getCurrentSensorValues());
-      LOGGER.log(Level.INFO, value);
-    } catch (Exception ex) {
-      LOGGER.log(Level.ERROR, "Unable to parse json: " + ex.getMessage());
-    }
-    session.getAsyncRemote().sendObject("test", result -> {
-      if (result.getException() != null) {
-        LOGGER.log(Level.ERROR, "Unable to send message: " + result.getException());
-      }
-    });
-  }
-
-  @OnClose
-  public void onClose(Session session) {
-    session.getAsyncRemote().sendObject("Close Session", result -> {
-      if (result.getException() != null) {
-        LOGGER.log(Level.ERROR, "Unable to send message: " + result.getException());
-      }
-    });
-  }
-
-  @OnError
-  public void onError(Session session, Throwable throwable) {
-    session.getAsyncRemote().sendObject("Error Session", result -> {
-      if (result.getException() != null) {
-        LOGGER.log(Level.ERROR, "Unable to send message: " + result.getException());
+  public void onOpen(Session session) {
+    managedExecutor.submit(() -> {
+      ObjectMapper objectMapper = new ObjectMapper();
+      try {
+        transactionManager.begin();
+        String jsonObject = objectMapper.writeValueAsString(sensorService.getCurrentSensorValues());
+        transactionManager.commit();
+        session.getAsyncRemote().sendObject(jsonObject, result -> {
+          if (result.getException() != null) {
+            LOGGER.log(Level.ERROR, "Unable to send message: " + result.getException());
+          }
+        });
+      } catch (Exception ex) {
+        LOGGER.log(Level.ERROR, "Unable to send message: " + ex.getMessage());
       }
     });
   }
