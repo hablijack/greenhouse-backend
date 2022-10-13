2f5f541042bf2ec51dbb1aa26cf4fd0f693d8da2
hablijack <christoph.habel@posteo.de>
2022-10-13 10:32:40 +0200

    1.0.18: added sensor crud operation functionality

--- src/main/java/de/hablijack/greenhouse/api/sensor/SensorResouce.java
+++ src/main/java/de/hablijack/greenhouse/api/sensor/SensorResouce.java
@@ -4,7 +4,9 @@ import de.hablijack.greenhouse.entity.Sensor;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.List;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,5 +19,22 @@ public class SensorResouce {
   @SuppressFBWarnings(value = "", justification = "Security is another Epic and on TODO")
   public List<Sensor> getAllSensors() {
     return Sensor.listAll();
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/sensor/{identifier}")
+  @SuppressFBWarnings(value = "", justification = "Security is another Epic and on TODO")
+  public List<Sensor> getOneSensor(@PathParam("identifier") String identifier) {
+    return Sensor.findById(identifier);
+  }
+
+  @PUT
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/sensor/{identifier}")
+  @SuppressFBWarnings(value = "", justification = "Security is another Epic and on TODO")
+  public Sensor updateOneSensor(@PathParam("identifier") String identifier, Sensor sensor) {
+    sensor.persist();
+    return sensor;
   }
 }
