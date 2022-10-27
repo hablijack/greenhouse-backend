739e0d70f00ec34ba37271f5b4947e736a235391
hablijack <christoph.habel@posteo.de>
2022-10-27 06:44:26 +0200

    1.0.25: lolz generics at its best

--- src/main/java/de/hablijack/greenhouse/service/DatabaseStatsService.java
+++ src/main/java/de/hablijack/greenhouse/service/DatabaseStatsService.java
@@ -2,7 +2,6 @@ package de.hablijack.greenhouse.service;
 
 import de.hablijack.greenhouse.api.pojo.TableSize;
 import java.math.BigInteger;
-import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -12,17 +11,17 @@ import javax.persistence.Query;
 @ApplicationScoped
 public class DatabaseStatsService {
   private static final String DB_SIZE_QUERY =
-      "SELECT pg_table_size('greenhouse.measurement') as measurementSizeByte, pg_table_size('greenhouse.relay_log') as relayLogSizeByte ; ";
+      "SELECT pg_table_size('greenhouse.measurement') as measurementSizeByte, pg_table_size('greenhouse.relay_log') as relayLogSizeByte";
   @Inject
   EntityManager entityManager;
 
   public TableSize getCurrentDatabaseSize() {
     Query query = entityManager.createNativeQuery(DB_SIZE_QUERY);
-    List<BigInteger[]> results = query.getResultList();
+    Object[] results = (Object[]) query.getSingleResult();
     TableSize tableSize = new TableSize();
-    if (results != null && results.size() > 0 && results.get(0).length >= 2) {
-      tableSize.setMeasurementSizeByte(results.get(0)[0]);
-      tableSize.setRelayLogSizeByte(results.get(0)[1]);
+    if (results != null && results.length > 0) {
+      tableSize.setMeasurementSizeByte((BigInteger) results[0]);
+      tableSize.setRelayLogSizeByte((BigInteger) results[1]);
     }
     return tableSize;
   }
