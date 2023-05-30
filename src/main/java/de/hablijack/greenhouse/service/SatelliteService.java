9ae8f942392fbbf0b6663dee9d26f40ddf3b6254
hablijack <christoph.habel@posteo.de>
2023-05-30 14:29:05 +0200

    2.5.6: fixed wait time for new image

--- src/main/java/de/hablijack/greenhouse/service/SatelliteService.java
+++ src/main/java/de/hablijack/greenhouse/service/SatelliteService.java
@@ -20,7 +20,7 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 @ApplicationScoped
 public class SatelliteService {
 
-  public static final int CAMERA_SNAPSHOT_WAIT_TIME = 5;
+  public static final int CAMERA_SNAPSHOT_WAIT_TIME = 7;
   private static final Logger LOGGER = Logger.getLogger(SatelliteService.class.getName());
   private static final Long CONNECT_TIMEOUT = 10000L;
   private static final Long READ_TIMEOUT = 10000L;
@@ -60,18 +60,19 @@ public class SatelliteService {
   @SuppressFBWarnings(value = {"CRLF_INJECTION_LOGS", "RCN_REDUNDANT_NULLCHECK_OF_NONNULL_VALUE"})
   public boolean savePictureToDatabase() {
     Satellite greenhouseCamera = Satellite.findByIdentifier("greenhouse_cam");
-    boolean success = false;
     if (greenhouseCamera != null && greenhouseCamera.online) {
       CameraPicture picture = CameraPicture.findExistingOrCreteNew();
       try {
         satelliteClient = createSatelliteClient(greenhouseCamera.ip);
       } catch (MalformedURLException error) {
         LOGGER.warning(error.getMessage());
+        return false;
       }
       try {
         File file = satelliteClient.savePicture();
         if (file.length() < MIN_FILE_SIZE) {
           LOGGER.warning("Picture not saved! Image is too small...");
+          return false;
         }
         try (FileInputStream readStream = new FileInputStream(file)) {
           picture.imageByte = readStream.readAllBytes();
@@ -81,13 +82,14 @@ public class SatelliteService {
           LOGGER.warning(error.getMessage());
         } catch (IOException error) {
           LOGGER.warning(error.getMessage());
+          return false;
         }
-        success = true;
       } catch (Exception error) {
         LOGGER.warning(error.getMessage());
+        return false;
       }
     }
-    return success;
+    return true;
   }
 
 }
