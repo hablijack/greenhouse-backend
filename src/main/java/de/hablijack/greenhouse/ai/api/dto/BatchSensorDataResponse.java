c54edd3ef77c65422ac898bb5408c386951fcb82
hablijack <christoph.habel@posteo.de>
2026-06-05 11:53:37 +0200

    fix: pipeline

--- src/main/java/de/hablijack/greenhouse/ai/api/dto/BatchSensorDataResponse.java
+++ src/main/java/de/hablijack/greenhouse/ai/api/dto/BatchSensorDataResponse.java
@@ -1,9 +1,11 @@
 package de.hablijack.greenhouse.ai.api.dto;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.util.Map;
 
 @RegisterForReflection
+@SuppressFBWarnings("URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
 public class BatchSensorDataResponse {
 
   public Map<String, AiRecommendationResponse> plants;
